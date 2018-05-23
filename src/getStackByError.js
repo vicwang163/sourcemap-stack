5dff33f99862db55708386ad0612cae2b1811730
fengyang <fengyang@daojia.com>
2018-05-23 14:33:10 +0800

    fix filepath带?的情况

--- src/getStackByError.js
+++ src/getStackByError.js
@@ -4,7 +4,8 @@ var path = require('path')
 */
 export function getStackByError (str) {
   let result = []
-  str.replace(/https?:\/\/[\w\d:\/\.\-]+/g, function (value) {
+  // fix filepath带?的情况，例如http://static.daojia.com/assets/other/test/debug_page/bug.min.js?v=1:1:102这种情况
+  str.replace(/https?:\/\/[\w\d:\/\.\-?=]+/g, function (value) {
     if (/^http/.test(value)) {
       result.push(value)
     }
@@ -14,6 +15,12 @@ export function getStackByError (str) {
     let column = arr.pop()
     let row = arr.pop()
     let filepath = path.dirname(item) + '/' + arr.join('')
+     // fix filepath带?的情况，例如http://static.daojia.com/assets/other/test/debug_page/bug.min.js?v=1:1:102这种情况
+    let qindex = filepath.indexOf('?');
+    if (qindex > -1) {
+      filepath = filepath.substring(0, qindex);
+    }
+
     let html = !/\.js$/.test(filepath)
     return {filepath, row, column, html}
   })
